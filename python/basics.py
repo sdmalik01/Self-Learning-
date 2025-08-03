99f0f5f669765c1b2ac85ae2193d75bb18f3935e
sdmalik01 <sdmalikwork01@gmail.com>
2025-08-03 20:33:19 +0530

    "Just checking workflow"

---
+++ python/basics.py
@@ -0,0 +1,15 @@
+# Basics syntax and things around python: such as variables, user inputs, loops, conditional statements, etc. 
+# Variables:
+number = 21
+string = "Malik"
+floatNumber = 26.58
+#To check the data type of the variable 
+print(type(number)) #Output: <class 'int'>
+
+#To assign multiple values:
+x, y, z = 21, "malik", 25.89
+'''Here x = 21, y = Malik, z = 25.89'''
+
+# Data type casting: refers to changing the data type of the variable
+x = 21
+print(type(string(x)))
