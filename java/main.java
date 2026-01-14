d1bf481ba9a7e97f414b41f496fc623c21a9e8ae
Sayyad Malik <sdmalikwork01@gmail.com>
2026-01-14 14:24:51 +0530

    Implement array operations
    Added ArrayOperations class to perform array operations.

--- java/main.java
+++ java/main.java
@@ -35,3 +35,77 @@ class Demo{
 }
 
 
+/*
+Java code to perform practical 3: 
+1. Finding the minimum and maximum value in the array
+2. Count even numbers present in the array
+3. Count the target number count in the array given by the user
+*/
+
+import java.util.Scanner;
+
+class ArrayOperations {
+    public static void main(String[] args) {
+        int[] numbers = {3, 12, 10, 52, 12, 45, 474, 52, 8979, 63};
+
+        System.out.print("The array is: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.print("\n");
+
+        findsmallestLargest(numbers);
+        System.out.print("\n");
+
+        CountEven(numbers);
+        System.out.print("\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to count occurrence: ");
+        int targetNumber = scanner.nextInt();
+
+        CountOccurence(numbers, targetNumber);
+        scanner.close();
+    }
+
+    public static void findsmallestLargest(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Smallest number in the array is: " + min);
+        System.out.println("Largest number in the array is: " + max);
+    }
+
+    public static void CountEven(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+        System.out.println("Total count of even numbers in the array is: " + count);
+    }
+
+    public static void CountOccurence(int[] arr, int target) {
+        int count_target = 0;
+        for (int num : arr) {
+            if (num == target) {
+                count_target++;
+            }
+        }
+        System.out.println("The target number count in the array is: " + count_target);
+    }
+}
+
+
+
+
